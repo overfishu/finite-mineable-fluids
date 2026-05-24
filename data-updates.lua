7cfcf7ac753d3acff31a0b57d8971aa282247510
Guillaume MRX <guillaume.mrx@gmail.com>
2026-05-24 19:59:39 +0200

    mod: data-updates.lua comments

--- data-updates.lua
+++ data-updates.lua
@@ -1,8 +1,8 @@
--- 			infinite	mining_time	amount_min	amount_max
--- crude-oil		true		1		10		10
--- sulfuric-acid	true		1		10		10
--- lithium-brine	false		1		 1		 1
--- fluorine		true		1		 1		 1
+-- 					infinite	mining_time	amount_min	amount_max
+-- crude-oil		true		1			10			10
+-- sulfuric-acid	true		1			10			10
+-- lithium-brine	false		1		 	 1			 1
+-- fluorine			true		1		 	 1		 	 1
 
 for _, resource in pairs(data.raw.resource) do
 	if resource.infinite then
@@ -10,8 +10,8 @@ for _, resource in pairs(data.raw.resource) do
 		for _, result in pairs(resource.minable.results) do
 			resource.minable.mining_time = resource.minable.mining_time / result.amount_min
 			result.amount_min = result.amount_min * resource.minable.mining_time
-          		result.amount_max = result.amount_max * resource.minable.mining_time
-          		-- error(string.format("mining time: %f, amount min: %i, amount max: %i", resource.minable.mining_time, result.amount_min, result.amount_max))
+          	result.amount_max = result.amount_max * resource.minable.mining_time
+          	-- error(string.format("mining time: %f, amount min: %i, amount max: %i", resource.minable.mining_time, result.amount_min, result.amount_max))
           	end
 	end
 end
